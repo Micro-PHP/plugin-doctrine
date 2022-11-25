2bb15af8aba0e526af168c9c3311b37b6a632bb4
Stanislau Komar <stanislau_komar@epam.com>
2022-11-25 20:48:58 +0400

    Fix command orm:clear-cache:region:collection

--- src/Console/CollectionRegionCommand.php
+++ src/Console/CollectionRegionCommand.php
@@ -2,7 +2,18 @@
 
 namespace Micro\Plugin\Doctrine\Console;
 
-class CollectionRegionCommand
-{
+use Doctrine\ORM\Tools\Console\Command as ORM;
+use Micro\Plugin\Doctrine\DoctrineFacadeInterface;
 
+class CollectionRegionCommand extends ORM\ClearCache\CollectionRegionCommand
+{
+    /**
+     * @param DoctrineFacadeInterface $doctrineFacade
+     */
+    public function __construct(
+        DoctrineFacadeInterface $doctrineFacade
+    )
+    {
+        parent::__construct($doctrineFacade);
+    }
 }
